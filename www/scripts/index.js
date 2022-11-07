774ec61c37fb8ec0ca7a7ec3e18fdf2886363515
ChristopherJamesN <nady.christopher@gmail.com>
2022-11-07 08:58:03 -0600

    More tweaks to attempt to debug the white screen when navigating to external url

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,34 +1,38 @@
 var app = {
-    // Application Constructor
-    initialize: function() {
+  // Application Constructor
+  initialize: function () {
     this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        StatusBar.overlaysWebView(false);
-        StatusBar.backgroundColorByName('black');
+  },
+  bindEvents: function () {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  onDeviceReady: function () {
+    app.receivedEvent('deviceready');
+    StatusBar.overlaysWebView(false);
+    StatusBar.backgroundColorByName('black');
 
-        // Here, we redirect to the web site.
-        var targetUrl = "https://agile-reef-34726.herokuapp.com/";
-        var bkpLink = document.getElementById("bkpLink");
-        bkpLink.setAttribute("href", targetUrl);
-        bkpLink.text = targetUrl;
-        window.location.replace(targetUrl);
-},
-    // Note: This code is taken from the Cordova CLI template.
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    // Here, we redirect to the web site.
+    var targetUrl = 'https://agile-reef-34726.herokuapp.com/';
+    var bkpLink = document.getElementById('bkpLink');
+    console.log('attempting to redirect to ' + targetUrl);
+    console.log('bkplink attribute is ' + bkpLink);
+    bkpLink.setAttribute('href', targetUrl);
+    /*
+    bkpLink.text = targetUrl;
+    window.location.replace(targetUrl);
+    */
+  },
+  // Note: This code is taken from the Cordova CLI template.
+  receivedEvent: function (id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+    console.log('Received Event: ' + id);
+  },
 };
 
 app.initialize();
